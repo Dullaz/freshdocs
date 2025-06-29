cb29855b9b09a0fb67fa3163cff8bc36772c589b
Dullaz <dev@dullaz.com>
2025-06-29 14:49:03 +0200

    Release v1.3.0
    - Build binaries for all platforms
    - Update Homebrew formula

--- Formula/freshdocs.rb
+++ Formula/freshdocs.rb
@@ -1,25 +1,25 @@
 class Freshdocs < Formula
   desc "Keep your documentation as fresh as your code"
   homepage "https://github.com/dullaz/freshdocs"
-  version "v1.2.0"
+  version "v1.3.0"
   
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/dullaz/freshdocs/releases/download/v1.2.0/freshdocs-v1.2.0-darwin-arm64.tar.gz"
-      sha256 "bef4bfd96ea02a9f3fc7eb8946725ca7ddb6999a28da68fe89943ecdd0de805e"
+      url "https://github.com/dullaz/freshdocs/releases/download/v1.3.0/freshdocs-v1.3.0-darwin-arm64.tar.gz"
+      sha256 "2ddb8c094cf9641163e0886a961fabee6ccf12c8aa9cbc2b82251b629040aba8"
     else
-      url "https://github.com/dullaz/freshdocs/releases/download/v1.2.0/freshdocs-v1.2.0-darwin-amd64.tar.gz"
-      sha256 "ee2eab6b5785080b1fcc45b2ee24cc339eead4c64082d2538de7246e6193c44c"
+      url "https://github.com/dullaz/freshdocs/releases/download/v1.3.0/freshdocs-v1.3.0-darwin-amd64.tar.gz"
+      sha256 "50f504287c74df947ac03b1c6d7d6c28a33ddbece7562320e9c95e5dec602678"
     end
   end
   
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/dullaz/freshdocs/releases/download/v1.2.0/freshdocs-v1.2.0-linux-arm64.tar.gz"
-      sha256 "99a9a3a2be84b95faf3ed35f89457e7d05281ed4c92bfd3a59483ee037ae9582"
+      url "https://github.com/dullaz/freshdocs/releases/download/v1.3.0/freshdocs-v1.3.0-linux-arm64.tar.gz"
+      sha256 "5c7ffa9e9a2ef1d1da71a88b71ee6864f20d84af219227865a1d0133f9034804"
     else
-      url "https://github.com/dullaz/freshdocs/releases/download/v1.2.0/freshdocs-v1.2.0-linux-amd64.tar.gz"
-      sha256 "b229154702f0422bdc6aa3645d76007c28c60854ce46e6a7f2a2df665046c512"
+      url "https://github.com/dullaz/freshdocs/releases/download/v1.3.0/freshdocs-v1.3.0-linux-amd64.tar.gz"
+      sha256 "0011afe4b3edfcbfe96f12b2c3e1cd89dee95b641e13b4fdd1834c237f6da709"
     end
   end
   
